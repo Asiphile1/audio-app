f72ebf6e4a6c3c97cbe5fb446d2fc3a3f617c646
asiphile <asiphilemthethethwa@gmail.com>
2024-11-14 16:03:28 +0200

    initial app entry added

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Button, FlatList } from "react-native";
+import AudioRecorder from "./components/AudioRecorder";
+import AudioList from "./components/AudioList";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import SearchBar from "./components/SearchBar";
 
 export default function App() {
+  const [audioNotes, setAudioNotes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    loadAudioNotes();
+  }, []);
+
+  const loadAudioNotes = async () => {
+    const storedNotes = await AsyncStorage.getItem("audioNotes");
+    if (storedNotes) {
+      setAudioNotes(JSON.parse(storedNotes));
+    }
+  };
+
+  const saveAudioNotes = async (notes) => {
+    await AsyncStorage.setItem("audioNotes", JSON.stringify(notes));
+    setAudioNotes(notes);
+  };
+
+  const handleDelete = async (id) => {
+    const filteredNotes = audioNotes.filter((note) => note.id !== id);
+    saveAudioNotes(filteredNotes);
+  };
+
+  const addNewNote = (note) => {
+    const updatedNotes = [...audioNotes, note];
+    saveAudioNotes(updatedNotes);
+  };
+
+  const filteredNotes = audioNotes.filter((note) =>
+    note.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <AudioRecorder addNewNote={addNewNote} />
+      <AudioList audioNotes={filteredNotes} onDelete={handleDelete} />
     </View>
   );
 }
@@ -13,8 +51,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    backgroundColor: "#fff",
   },
 });
